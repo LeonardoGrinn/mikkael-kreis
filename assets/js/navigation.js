85a31a2f89e8ecba2af9f3c1a79c5b61a2788abc
LeonardoSolutio <leosolutio@gmail.com>
2020-06-18 01:36:53 -0500

    Sitio terminado

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.querySelector('.navigation__bar').style.height = "0";
+  document.querySelector('.navigation__bar').style.height = "100vh";
   document.querySelector('.navigation__btn-container').style.marginBottom = "0px";
 }
 
